c590a86403d376be61ce7137fb51980f057d640f
woleto <1059446337@qq.com>
2019-09-19 14:27:29 +0800

    V1.1 修改了创建图谱后直接进行查询会出现无法独占打开数据源的bug

--- src/main/java/www/supermap/model/iobjects/RecordSetEntity.java
+++ src/main/java/www/supermap/model/iobjects/RecordSetEntity.java
@@ -8,6 +8,7 @@ import com.supermap.data.Dataset;
 import com.supermap.data.DatasetVector;
 import com.supermap.data.Datasource;
 import com.supermap.data.DatasourceConnectionInfo;
+import com.supermap.data.Datasources;
 import com.supermap.data.EngineType;
 import com.supermap.data.FieldInfo;
 import com.supermap.data.FieldInfos;
@@ -17,51 +18,58 @@ import com.supermap.data.Recordset;
 import com.supermap.data.Workspace;
 
 public class RecordSetEntity {
-	
+
 	private String recordId;
-//	private Recordset recordSet;
-	private String dataDtoreDir; 
+	// private Recordset recordSet;
+	private String dataDtoreDir;
 	private String entityType;
-	//字段属性
+	// 字段属性
 	private Point2D point;
 	private String mingCheng;
-	public RecordSetEntity(String recordId,String dataDtoreDir,String entityType) {
+
+	public RecordSetEntity(String recordId, String dataDtoreDir, String entityType) {
 		// TODO Auto-generated constructor stub
 		this.recordId = recordId;
 		this.dataDtoreDir = dataDtoreDir;
 		this.entityType = entityType;
-//		this.recordSet = getRecordSetById(recordId,dataDtoreDir,entityType);
-		getInfoByRecordId(recordId,dataDtoreDir,entityType);
+		// this.recordSet = getRecordSetById(recordId,dataDtoreDir,entityType);
+		getInfoByRecordId(recordId, dataDtoreDir, entityType);
+	}
+
+	public RecordSetEntity(Recordset recordSet) {
+		getRequiredInfo(recordSet);
 	}
 
 	public Point2D getPoint() {
 		return point;
 	}
-	
+
 	public String getMingCheng() {
 		return mingCheng;
 	}
 
-	private static Recordset getRecordSetById(String recordId, String dataDtoreDir, String entityType){
+	private static Recordset getRecordSetById(String recordId, String dataDtoreDir, String entityType) {
 		String[] idSplits = recordId.split("_");
 		String dataSourceId = idSplits[0];
 		String dataSetId = idSplits[1];
 		int recordIndex = Integer.valueOf(idSplits[2]);
-		//读取数据
+		// 读取数据
 		Workspace workSpace = new Workspace();
 		DatasourceConnectionInfo dataSourceConnectionInfo = new DatasourceConnectionInfo();
-		dataSourceConnectionInfo.setServer(dataDtoreDir+File.separator+dataSourceId+".udb");
+		dataSourceConnectionInfo.setServer(dataDtoreDir + File.separator + dataSourceId + ".udb");
 		dataSourceConnectionInfo.setEngineType(EngineType.UDB);
 		Datasource dataSource = workSpace.getDatasources().open(dataSourceConnectionInfo);
-		Dataset dataSet = dataSource.getDatasets().get(dataSetId+"_"+entityType);
+		Dataset dataSet = dataSource.getDatasets().get(dataSetId + "_" + entityType);
 		DatasetVector dataSetVector = (DatasetVector) dataSet;
 		Recordset recordSet = dataSetVector.getRecordset(false, CursorType.STATIC);
 		recordSet.moveTo(recordIndex);
 		dataSource.close();
 		return recordSet;
 	}
+
 	/**
 	 * 获得recordset中所需要的字段
+	 * 
 	 * @param recordId
 	 * @param dataDtoreDir
 	 * @param entityType
@@ -72,47 +80,62 @@ public class RecordSetEntity {
 		String dataSourceId = idSplits[0];
 		String dataSetId = idSplits[1];
 		int recordIndex = Integer.valueOf(idSplits[2]);
-		//获得recordset
+		// 获得recordset
 		Workspace workSpace = new Workspace();
 		DatasourceConnectionInfo dataSourceConnectionInfo = new DatasourceConnectionInfo();
-		dataSourceConnectionInfo.setServer(dataDtoreDir+File.separator+dataSourceId+".udb");
+		dataSourceConnectionInfo.setServer(dataDtoreDir + File.separator + dataSourceId + ".udb");
 		dataSourceConnectionInfo.setEngineType(EngineType.UDB);
-		Datasource dataSource = workSpace.getDatasources().open(dataSourceConnectionInfo);
-		Dataset dataSet = dataSource.getDatasets().get(dataSetId+"_"+entityType);
-		DatasetVector dataSetVector = (DatasetVector) dataSet;
-		Recordset recordSet = dataSetVector.getRecordset(false, CursorType.STATIC);
-		recordSet.moveTo(recordIndex);
-		//通过recordset取出所需要的字段值
-		getRequiredInfo(recordSet);
-		dataSource.close();
+		// System.out.println(dataSourceId+"-"+dataSetId+"-"+recordIndex);
+		
+		Datasource dataSource = null;
+		try {
+			dataSource = workSpace.getDatasources().open(dataSourceConnectionInfo);
+		} catch (Exception e) {
+//			System.out.println(dataSourceId + "-" + dataSetId + "-" + recordIndex);
+//			System.out.println(recordId);
+		}
+		if (dataSource != null) {
+			Dataset dataSet = dataSource.getDatasets().get(dataSetId + "_" + entityType);
+			DatasetVector dataSetVector = (DatasetVector) dataSet;
+			Recordset recordSet = dataSetVector.getRecordset(false, CursorType.STATIC);
+			recordSet.moveTo(recordIndex);
+			// 通过recordset取出所需要的字段值
+			getRequiredInfo(recordSet);
+			dataSource.close();
+		}
+		// if(dataSource.isOpened()){
+		// System.out.println(dataSourceId+"-"+dataSetId+"-"+recordIndex+"数据集已打开----------------------------------------------");
+		// }
+
 	}
 
 	/**
 	 * 通过recordset来获取字段值
+	 * 
 	 * @param recordSet
 	 */
 	private void getRequiredInfo(Recordset recordSet) {
 		// TODO Auto-generated method stub
-		//取出实体位于的经纬度
+		// 取出实体位于的经纬度
 		Geometry geometry = recordSet.getGeometry();
 		this.point = geometry.getInnerPoint();
-		//获取位置信息
-//		this.weiZhi = recordSet.getFieldValue("wz"));
-//		this.quXian = recordSet.getFieldValue("qx"));
-		//不是所有的实体都有名称字段，因此首先检查名称字段，没有的话检查位置，再检查区县，再没有就直接用null代替
+		// 获取位置信息
+		// this.weiZhi = recordSet.getFieldValue("wz"));
+		// this.quXian = recordSet.getFieldValue("qx"));
+		// 不是所有的实体都有名称字段，因此首先检查名称字段，没有的话检查位置，再检查区县，再没有就直接用null代替
 		try {
-			this.mingCheng = (String) recordSet.getFieldValue("mc");		
+			this.mingCheng = (String) recordSet.getFieldValue("mc");
 		} catch (Exception e) {
 			// TODO: handle exception
 			try {
-				this.mingCheng = (String) recordSet.getFieldValue("wz");			
+				this.mingCheng = (String) recordSet.getFieldValue("wz");
 			} catch (Exception e2) {
 				// TODO: handle exception
-				try {		
-					this.mingCheng =  (String)recordSet.getFieldValue("qx");
+				try {
+					this.mingCheng = (String) recordSet.getFieldValue("qx");
 				} catch (Exception e3) {
 					// TODO: handle exception
-					this.mingCheng =null;
+					this.mingCheng = null;
 				}
 			}
 		}
@@ -125,5 +148,5 @@ public class RecordSetEntity {
 	public String toString() {
 		return "[point=" + point + ", mingCheng=" + mingCheng + "]";
 	}
-	
+
 }
--- src/main/java/www/supermap/utils/Iobjects.java
+++ src/main/java/www/supermap/utils/Iobjects.java
@@ -32,33 +32,36 @@ public class Iobjects {
 	public Iobjects() {
 		// TODO Auto-generated constructor stub
 	}
+
 	/**
 	 * 通过检查当前图谱源文件下存储的数据集，得到最后一个数据集的id
-	 * @return 
+	 * 
+	 * @return
 	 */
 	public static String getEndDataSetId(String storeDir) {
 		// TODO Auto-generated method stub
-		//目前只使用一个数据源，id为0
+		// 目前只使用一个数据源，id为0
 		String defaultStore = "0.udb";
 		Workspace workSpace = new Workspace();
 		DatasourceConnectionInfo datasourceConnectionInfo = new DatasourceConnectionInfo();
-		datasourceConnectionInfo.setServer(storeDir+File.separator+defaultStore);
+		datasourceConnectionInfo.setServer(storeDir + File.separator + defaultStore);
 		datasourceConnectionInfo.setEngineType(EngineType.UDB);
 		Datasource dataSource = null;
 		try {
-			//打开数据源
+			// 打开数据源
 			dataSource = workSpace.getDatasources().open(datasourceConnectionInfo);
 		} catch (javax.management.RuntimeErrorException e) {
 			// TODO: handle exception
 			dataSource = workSpace.getDatasources().create(datasourceConnectionInfo);
 		}
-		int id = dataSource.getDatasets().getCount()-1;
+		int id = dataSource.getDatasets().getCount() - 1;
 		dataSource.close();
-		return "0_D"+id;
+		return "0_D" + id;
 	}
 
 	/**
 	 * 从所有的数据集中过滤出指定类型的数据集
+	 * 
 	 * @param allDataSets
 	 * @param geoTypes
 	 * @return
@@ -66,31 +69,30 @@ public class Iobjects {
 	public static ArrayList<Dataset> filterDataSetByAssignGeoTypes(ArrayList<Dataset> allDataSets, String[] geoTypes) {
 		// TODO Auto-generated method stub
 		ArrayList<Dataset> filterDataSets = new ArrayList<Dataset>();
-		if ( geoTypes == null||geoTypes.length == 0 ) {
+		if (geoTypes == null || geoTypes.length == 0) {
 			for (Dataset dataSet : allDataSets) {
 				try {
-					Dataset dataSetVector = (DatasetVector)dataSet;
+					Dataset dataSetVector = (DatasetVector) dataSet;
 				} catch (Exception e) {
 					// TODO: handle exception
 					e.printStackTrace();
 					System.out.println("目前只支持矢量数据集，请指定矢量数据集或删除数据源中的所有非矢量数据集，然后再次尝试");
-				}			
+				}
 			}
 			filterDataSets = allDataSets;
-		} 
-		else {
+		} else {
 			for (String type : geoTypes) {
 				for (Dataset dataSet : allDataSets) {
-					//以数据集名字作为类型判别
-					if(dataSet.getName().equals(type)){
+					// 以数据集名字作为类型判别
+					if (dataSet.getName().equals(type)) {
 						try {
-							Dataset dataSetVector = (DatasetVector)dataSet;
+							Dataset dataSetVector = (DatasetVector) dataSet;
 						} catch (Exception e) {
 							// TODO: handle exception
 							e.printStackTrace();
-							System.out.println("目前只支持矢量数据集，检测到  "+type+" 为非矢量数据集，请删除该类型后重试 ");
+							System.out.println("目前只支持矢量数据集，检测到  " + type + " 为非矢量数据集，请删除该类型后重试 ");
 							System.exit(1);
-						}			
+						}
 						filterDataSets.add(dataSet);
 						break;
 					}
@@ -102,6 +104,7 @@ public class Iobjects {
 
 	/**
 	 * 通过字符串来获得包含的所有数据源,目前只实现了udb
+	 * 
 	 * @param dataSource
 	 * @return
 	 */
@@ -110,59 +113,63 @@ public class Iobjects {
 		ArrayList<Dataset> dataSets = new ArrayList<Dataset>();
 		Workspace workSpace = new Workspace();
 		DatasourceConnectionInfo datasourceConnectionInfo = new DatasourceConnectionInfo();
-		//要判断是否为smwu工作空间，udb数据源，shp文件，数据库连接字符串
-		if(dataSource.endsWith("smwu")){
-			//smwu工作空间
-		}else if(dataSource.endsWith("udb")){
-			//udb文件型数据源
+		// 要判断是否为smwu工作空间，udb数据源，shp文件，数据库连接字符串
+		if (dataSource.endsWith("smwu")) {
+			// smwu工作空间
+		} else if (dataSource.endsWith("udb")) {
+			// udb文件型数据源
 			datasourceConnectionInfo.setServer(dataSource);
 			datasourceConnectionInfo.setEngineType(EngineType.UDB);
 			Datasource datasource = workSpace.getDatasources().open(datasourceConnectionInfo);
 			Datasets allDataSets = datasource.getDatasets();
 			for (int i = 0; i < allDataSets.getCount(); i++) {
-				Dataset dataSet  = allDataSets.get(i);
-				dataSets.add(dataSet);				
+				Dataset dataSet = allDataSets.get(i);
+				dataSets.add(dataSet);
 			}
-		}else if(dataSource.endsWith("shp")){
-			//shp文件
-		}else if(dataSource.endsWith("shujuku~!@#$%^&*()_+")){
-			//预留给数据库
+		} else if (dataSource.endsWith("shp")) {
+			// shp文件
+		} else if (dataSource.endsWith("shujuku~!@#$%^&*()_+")) {
+			// 预留给数据库
 		}
 		return dataSets;
 	}
 
 	/**
 	 * 获得下一个数据集要存的id，如果需要则创建新的数据源
+	 * 
 	 * @param currentSetStartId
 	 * @param originDataStorePath
 	 * @return
 	 */
 	public static String getNextDataSetId(String currentSetStartId, String originDataStorePath) {
 		// TODO Auto-generated method stub
-		String [] idSplits = currentSetStartId.split("_");
+		String[] idSplits = currentSetStartId.split("_");
 		int preDataSetId = Integer.valueOf(idSplits[1].substring(1));
-		int currentDataSetId = preDataSetId+1;
-		return idSplits[0]+"_D"+currentDataSetId;
+		int currentDataSetId = preDataSetId + 1;
+		return idSplits[0] + "_D" + currentDataSetId;
 	}
 
 	/**
 	 * 从存储到图谱数据源的数据集中读取数据
+	 * 
 	 * @param storeDataSetsIds
 	 * @return
 	 */
-	public static ArrayList<GeoObjectEntity> getGisDataFromDataSet(String dataSourcePath,String wholeDataSetId) {
+	public static ArrayList<GeoObjectEntity> getGisDataFromDataSet(String dataSourcePath, String wholeDataSetId) {
 		// TODO Auto-generated method stub
 		String[] idSplits = wholeDataSetId.split("_");
 		String dataSourceId = idSplits[0];
-		String dataSetId  = idSplits[1];
+		String dataSetId = idSplits[1];
 		String entityType = idSplits[2];
-		String recordPrefix = dataSourceId+"_"+dataSetId+"_";
-		ArrayList<GeoObjectEntity> geoEntities = getGeoEntityFromDataSet(dataSourcePath,dataSourceId,dataSetId,entityType,recordPrefix);	
+		String recordPrefix = dataSourceId + "_" + dataSetId + "_";
+		ArrayList<GeoObjectEntity> geoEntities = getGeoEntityFromDataSet(dataSourcePath, dataSourceId, dataSetId,
+				entityType, recordPrefix);
 		return geoEntities;
 	}
-	
+
 	/**
 	 * 从指定数据集中读取数据
+	 * 
 	 * @param dataSourceId
 	 * @param dataSetId
 	 * @param entityType
@@ -170,44 +177,45 @@ public class Iobjects {
 	 * @param dataSourcePath
 	 * @return
 	 */
-	private static ArrayList<GeoObjectEntity> getGeoEntityFromDataSet(String dataSourcePath,String dataSourceId, String dataSetId,String entityType, String recordPrefix) {
+	private static ArrayList<GeoObjectEntity> getGeoEntityFromDataSet(String dataSourcePath, String dataSourceId,
+			String dataSetId, String entityType, String recordPrefix) {
 		// TODO Auto-generated method stub
 		ArrayList<GeoObjectEntity> geoEntities = new ArrayList<GeoObjectEntity>();
-		//打开数据源
+		// 打开数据源
 		Workspace workSpace = new Workspace();
 		DatasourceConnectionInfo dataSourceConnectionInfo = new DatasourceConnectionInfo();
-		dataSourceConnectionInfo.setServer(dataSourcePath+File.separator+dataSourceId+".udb");
+		dataSourceConnectionInfo.setServer(dataSourcePath + File.separator + dataSourceId + ".udb");
 		dataSourceConnectionInfo.setEngineType(EngineType.UDB);
 		Datasource dataSource = workSpace.getDatasources().open(dataSourceConnectionInfo);
-		Dataset dataSet = dataSource.getDatasets().get(dataSetId+"_"+entityType);
+		Dataset dataSet = dataSource.getDatasets().get(dataSetId + "_" + entityType);
 		DatasetVector dataSetVector = (DatasetVector) dataSet;
 		Recordset recordSet = dataSetVector.getRecordset(false, CursorType.STATIC);
 		DatasetType dataSetType = dataSetVector.getType();
-		//处理点线面
-		if(dataSetType.equals(DatasetType.POINT)){
+		// 处理点线面
+		if (dataSetType.equals(DatasetType.POINT)) {
 			for (int i = 0; i < recordSet.getRecordCount(); i++) {
-				GeoPoint point = (GeoPoint)recordSet.getGeometry();
-				String entityId = recordPrefix+recordSet.getID();
+				GeoPoint point = (GeoPoint) recordSet.getGeometry();
+				String entityId = recordPrefix + recordSet.getID();
 				PointObjectEntity pointEntity = new PointObjectEntity(point, entityType, entityId);
 				geoEntities.add(pointEntity);
 				recordSet.moveNext();
-			}		
-		}else if(dataSetType.equals(DatasetType.LINE)){
+			}
+		} else if (dataSetType.equals(DatasetType.LINE)) {
 			for (int i = 0; i < recordSet.getRecordCount(); i++) {
-				GeoLine line = (GeoLine)recordSet.getGeometry();
-				String entityId = recordPrefix+recordSet.getID();
+				GeoLine line = (GeoLine) recordSet.getGeometry();
+				String entityId = recordPrefix + recordSet.getID();
 				LineObjectEntity lineEntity = new LineObjectEntity(line, entityType, entityId);
 				geoEntities.add(lineEntity);
 				recordSet.moveNext();
 			}
-		}else if(dataSetType.equals(DatasetType.REGION)){
+		} else if (dataSetType.equals(DatasetType.REGION)) {
 			for (int i = 0; i < recordSet.getRecordCount(); i++) {
-				GeoRegion region = (GeoRegion)recordSet.getGeometry();
-				String entityId = recordPrefix+recordSet.getID();
+				GeoRegion region = (GeoRegion) recordSet.getGeometry();
+				String entityId = recordPrefix + recordSet.getID();
 				RegionObjectEntity regionEntity = new RegionObjectEntity(region, entityType, entityId);
 				geoEntities.add(regionEntity);
 				recordSet.moveNext();
-			}	
+			}
 		}
 		dataSource.close();
 		return geoEntities;
@@ -215,41 +223,44 @@ public class Iobjects {
 
 	/**
 	 * 将传入的gis数填补，转换成适存储的grid
+	 * 
 	 * @param gisData
 	 * @param gridLevel
 	 * @return
 	 */
-	public static ArrayList<ObjectGrid> getKnowledgeGraphModelFromObjects(ArrayList<GeoObjectEntity> gisData, int gridLevel) {
+	public static ArrayList<ObjectGrid> getKnowledgeGraphModelFromObjects(ArrayList<GeoObjectEntity> gisData,
+			int gridLevel) {
 		// TODO Auto-generated method stub
 		for (GeoObjectEntity geoEntity : gisData) {
-			//处理点实体
+			// 处理点实体
 			if (geoEntity instanceof PointObjectEntity) {
 				PointObjectEntity pointEntity = (PointObjectEntity) geoEntity;
 				pointEntity.setCellLevel(gridLevel);
 				GeoPoint point = pointEntity.getPoint();
 				ArrayList<Long> cellIds = S2.getGeoPointCoveringCell(point, gridLevel);
-				pointEntity.setCellIds(cellIds);		
-			}else if (geoEntity instanceof LineObjectEntity) {
+				pointEntity.setCellIds(cellIds);
+			} else if (geoEntity instanceof LineObjectEntity) {
 				LineObjectEntity lineEntity = (LineObjectEntity) geoEntity;
 				lineEntity.setCellLevel(gridLevel);
 				GeoLine line = lineEntity.getLine();
 				ArrayList<Long> cellIds = S2.getGeoLineCoveringCells(line, gridLevel);
-				lineEntity.setCellIds(cellIds);		
+				lineEntity.setCellIds(cellIds);
 			} else if (geoEntity instanceof RegionObjectEntity) {
 				RegionObjectEntity regionEntity = (RegionObjectEntity) geoEntity;
 				regionEntity.setCellLevel(gridLevel);
 				GeoRegion region = regionEntity.getMultiPolygon();
 				ArrayList<Long> cellIds = S2.getGeoRegionCoveringCells(region, gridLevel);
-				regionEntity.setCellIds(cellIds);	
+				regionEntity.setCellIds(cellIds);
 			}
 		}
-		//完善实体信息后生成用于存储的网格实体
+		// 完善实体信息后生成用于存储的网格实体
 		ArrayList<ObjectGrid> grids = getGridModelFromObjects(gisData);
 		return grids;
 	}
 
 	/**
 	 * 使用完整的地理实体生成网格模型
+	 * 
 	 * @param gisData
 	 * @return
 	 */
@@ -266,7 +277,7 @@ public class Iobjects {
 			ArrayList<GeoObjectEntity> entityies = new ArrayList<GeoObjectEntity>();
 			for (GeoObjectEntity geoEntity : gisData) {
 				for (Long cellId : geoEntity.getCellIds()) {
-					if(cellId.longValue()==id.longValue()){
+					if (cellId.longValue() == id.longValue()) {
 						entityies.add(geoEntity);
 						break;
 					}
@@ -277,24 +288,57 @@ public class Iobjects {
 		}
 		return grids;
 	}
-	
+
 	/**
 	 * 通过查询出的id去文件里取RecordSet
+	 * 
 	 * @param idResults
 	 * @param originDataStorePath
 	 * @return
 	 */
-	public static HashMap<String, ArrayList<RecordSetEntity>> getRecordSetFromIds(HashMap<String, ArrayList<String>> idResults, String originDataStorePath) {
+	public static HashMap<String, ArrayList<RecordSetEntity>> getRecordSetFromIds(
+			HashMap<String, ArrayList<String>> idResults, String originDataStorePath) {
 		// TODO Auto-generated method stub
-		HashMap<String, ArrayList<RecordSetEntity>> idAndRecordSets= new HashMap<String, ArrayList<RecordSetEntity>>();
-		for (Entry<String, ArrayList<String>> entry : idResults.entrySet()) {
-			String entityType = entry.getKey();
+		HashMap<String, ArrayList<RecordSetEntity>> idAndRecordSets = new HashMap<String, ArrayList<RecordSetEntity>>();
+		/**
+		 * V1.0版本的处理方式：每读一条记录就要打开和关闭一次数据源，已作废
+		 */
+//		for (Entry<String, ArrayList<String>> entry : idResults.entrySet()) {
+//			String entityType = entry.getKey();
+//			ArrayList<RecordSetEntity> recordSetEntities = new ArrayList<RecordSetEntity>();
+//			for (String recordId : entry.getValue()) {
+//				RecordSetEntity recordSetEntity = new RecordSetEntity(recordId, originDataStorePath, entityType);
+//				if (recordSetEntity.getPoint() != null) {
+//					recordSetEntities.add(recordSetEntity);
+//				}
+//			}
+//			idAndRecordSets.put(entityType, recordSetEntities);
+//		}
+
+		for (String type : idResults.keySet()) {
+			ArrayList<String> recordIds = idResults.get(type);
+			//获得当前类型存在哪个数据源、数据集
+			String[] idSplits = recordIds.get(0).split("_");
+			String dataSourceId = idSplits[0];
+			String dataSetId = idSplits[1];
+			Workspace workSpace = new Workspace();
+			DatasourceConnectionInfo dataSourceConnectionInfo = new DatasourceConnectionInfo();
+			dataSourceConnectionInfo.setServer(originDataStorePath + File.separator + dataSourceId + ".udb");
+			dataSourceConnectionInfo.setEngineType(EngineType.UDB);
+			Datasource dataSource = workSpace.getDatasources().open(dataSourceConnectionInfo);
+			Dataset dataSet = dataSource.getDatasets().get(dataSetId + "_" + type);
+			DatasetVector dataSetVector = (DatasetVector) dataSet;
+			Recordset recordSet = dataSetVector.getRecordset(false, CursorType.STATIC);
 			ArrayList<RecordSetEntity> recordSetEntities = new ArrayList<RecordSetEntity>();
-			for (String recordId : entry.getValue()) {
-				RecordSetEntity recordSetEntity = new RecordSetEntity(recordId, originDataStorePath, entityType);
+			for (String recordId : recordIds) {
+				int recordIndex = Integer.valueOf(recordId.split("_")[2]);
+				recordSet.moveTo(recordIndex);
+				// 通过recordset取出所需要的字段值
+				RecordSetEntity recordSetEntity = new RecordSetEntity(recordSet);
 				recordSetEntities.add(recordSetEntity);
 			}
-			idAndRecordSets.put(entityType, recordSetEntities);
+			idAndRecordSets.put(type, recordSetEntities);
+			dataSource.close();
 		}
 		return idAndRecordSets;
 	}
